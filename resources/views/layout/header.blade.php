0ad0b0ca58f3dc8887ee12fd1582b40cfe911ebf
Nicolas Lamblin <nicolas.lamblin2@gmail.com>
2017-11-28 16:41:49 +0100

    Define User models + create readme

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="{{ url('/') }}">Home</a></li>
+            <li><a href="{{ url('/') }}">Home</a></li>
             {{--<li><a href="#">Page 1</a></li>
             <li><a href="#">Page 2</a></li>--}}
         </ul>
