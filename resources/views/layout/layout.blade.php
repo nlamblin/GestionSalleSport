4895b00d3f1bccfd8c157a5a67e10d00e7a8c13f
Nicolas Lamblin <nicolas.lamblin2@gmail.com>
2017-11-28 14:12:51 +0100

    Adding css and js files / layout, header, footer creation

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CSI</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+</head>
+
+<body>
+
+    @include('layout.header')
+
+    @yield('content')
+
+    @include('layout.footer')
+
+</body>
