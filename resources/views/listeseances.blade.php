540f654bde47ccb097acd02fc4469e4ef338b2fb
Nicolas Lamblin <nicolas.lamblin2@gmail.com>
2017-12-15 18:14:08 +0100

    Modif blade seances

--- resources/views/listeseances.blade.php
+++ resources/views/listeseances.blade.php
@@ -4,71 +4,29 @@
 
 
 @section('content')
+    <div class="container">
+        <h1 class="page-header">
+            Séances
+        </h1>
 
-    <!-- Header Carousel -->
-    <header id="myCarousel" class="carousel slide">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="icon-prev"></span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="icon-next"></span>
-        </a>
-    
-    
-
-           <div class="row">
+        <div class="row">
             <!-- Menu de selection de l'activite -->
 
             <label for="select_activite" class="col-md-4 control-label">Choisissez une activité : </label>
-                    <div class="col-md-6">
-                        <select id="select_activite" class="form-control" name="select_activite">
-            @foreach($listeactivite as $value)
-                            <option value=$value.id_activite>{{$value->nom_activite}}</option>
-            @endforeach             
-                        </select>
-                </div>
+            <div class="col-md-6">
+                <select id="select_activite" class="form-control" name="select_activite">
+                    @foreach($listeactivite as $value)
+                        <option value=$value.id_activite>{{$value->nom_activite}}</option>
+                    @endforeach
+                </select>
+            </div>
 
-
-           </div>
-
-       </header>
-        <div class="container">
+        </div>
        <!-- Project One -->
        @foreach($listeseance as $value)
-       <!-- Project one -->
         <div class="row">
             <div class="col-md-7">
-                <a href="portfolio-item.html">
+                <a href="#">
                     <img class="img-responsive img-hover" src="http://placehold.it/700x300" alt="">
                 </a>
             </div>
