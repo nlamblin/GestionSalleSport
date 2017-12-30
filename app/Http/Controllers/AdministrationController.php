1ae476de9366491f9765605bdd227317ce90d12b
Anaïs Thiriot <anaisthiriot@hotmail.fr>
2017-12-30 13:10:13 +0100

    Création page administration (onglet, controller, view, route)

---
+++ app/Http/Controllers/AdministrationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AdministrationController extends Controller
+{
+    public function index() {
+        return view('administration');
+    }
+}
